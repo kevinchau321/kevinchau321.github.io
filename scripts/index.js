c34c241a59614fb1593d75bd6f9684a8a59c2e7e
kevinchau321 <kevinchau321@berkeley.edu>
2017-03-03 02:00:43 -0800

    jquery to replace base address

--- scripts/index.js
+++ scripts/index.js
@@ -40,8 +40,9 @@ defer(function() {
         x.open('GET', cors_api_url + 'github.com/kevinchau321');
         x.onload = x.onerror = function() {
             var content = $(x.responseText).find('.js-pinned-repos-reorder-container');
-            // $(content).find('.contributions-setting').remove();
-            // $(content).find('.float-left.text-gray').remove();
+            $(content).text(function () {
+              return $(this).text().replace("kevinchau321.github.io", "www.github.com");
+            });​​​​​
             $('.col-9').append(content);
         };
         x.send(options.data);
