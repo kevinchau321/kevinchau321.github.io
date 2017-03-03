fdb5cf4af86b7dc724391b2350dd4e50fc584578
kevinchau321 <kevinchau321@berkeley.edu>
2017-03-03 02:05:48 -0800

    change to one-liner

--- scripts/index.js
+++ scripts/index.js
@@ -40,9 +40,7 @@ defer(function() {
         x.open('GET', cors_api_url + 'github.com/kevinchau321');
         x.onload = x.onerror = function() {
             var content = $(x.responseText).find('.js-pinned-repos-reorder-container');
-            $(content).text(function () {
-              return $(this).text().replace("kevinchau321.github.io", "www.github.com");
-            });​​​​​
+            $(content).text(function () { return $(this).text().replace("kevinchau321.github.io", "www.github.com"); });​​​​​
             $('.col-9').append(content);
         };
         x.send(options.data);
